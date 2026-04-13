ea61b8d6ffa27dd95cea1199267a5fe9650ed596
stepanbogdan43 <stepanbogdan43@gmail.com>
2026-04-13 11:07:18 +0300

    guessnumber

---
+++ ghiceste numarul/code1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+    int randomnumber, ghicire, incercari = 10;
+
+    randomnumber =rand() % 101 ;
+
+    cout << "=== Guess the Number ==="<<endl;
+    cout << "Am ales un numar intre 1 si 100."<<endl;
+    cout << "Ai 10 incercari"<<endl;
+    do {
+
+        cout << "Introdu un numar: "<<endl;
+        cin >> ghicire;
+        incercari--;
+
+
+        if (ghicire > randomnumber) {
+
+            cout << "Prea mare!"<<endl;
+            cout << "Mai ai "<<incercari<<" incercari"<<endl;
+
+        } else if (ghicire < randomnumber) {
+
+            cout << "Prea mic!"<<endl;
+            cout << "Mai ai "<<incercari<<" incercari"<<endl;
+
+        } else {
+
+            cout << "Felicitari! Ai ghicit numarul in " <<10-incercari << " incercari!"<<endl;
+        }
+
+    } 
+    while (ghicire != randomnumber && incercari>0);
+
+    return 0;
+}
+ 
